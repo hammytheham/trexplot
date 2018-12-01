babfefb8541da4442170cff9accb332e3c0cc81f
hamish <h.alastair.r@gmail.com>
2018-12-01 16:33:19 -0700

    Updates to fig_return

--- trex_fig_return.py
+++ trex_fig_return.py
@@ -4,8 +4,8 @@ import trexplot as tp
 
 def main():
     plots=tp.main()
-    print(plots)
-#    fig,ax=plots['fig_TopPerm_X(m2)pcolor']
+#    print(plots)
+    fig,ax=plots[-1]['xsec_y_user_val2dolomitepcolor']
 #    fig,ax=plots['fig_TopTemperature(C)pcolor']
     #fig,ax=plots['fig_xsec_y_halfTemperature(C)pcolor']
 #
@@ -14,6 +14,6 @@ def main():
 #    ax.set_xlim(300,500)
 #    ax.set_ylim(200,800)
     #fig.savefig('/Users/hamish/Desktop/TREXWORK/testfig')
-#    fig.show()
+    fig.show()
 if __name__ == "__main__":
     main()
